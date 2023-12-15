7dcd8411fba65f87eb114ec12b96d4f7a0aaba86
tharindug99 <tngodage@std.appsc.sab.ac.lk>
2023-12-15 18:39:04 +0530

    new changes

--- frontend/components/header/Header.jsx
+++ frontend/components/header/Header.jsx
@@ -1,62 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { BiMenu } from 'react-icons/bi';
 import logo from '../../src/assets/images/logo.png';
-import {Link, NavLink} from 'react-router-dom';
-import userimg from '../../src/assets/images/avatar.png'
-import {useState, useEffect} from 'react';
+import userimg from '../../src/assets/images/avatar.png';
+
 
 const navLinks = [
   {
-    path:'./home',
-    display:'Home'
+    path: '/home',
+    display: 'Home',
   },
   {
-    path:'./doctors',
-    display:'Find a Doctor'
+    path: '/doctors',
+    display: 'Find a Doctor',
   },
   {
-    path:'/services',
-    display:'Services'
+    path: '/services',
+    display: 'Services',
   },
   {
-    path:'./contact',
-    display:'Contact'
-  }
-]
+    path: '/contact',
+    display: 'Contact',
+  },
+];
 
 const Header = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header');
+      } else {
+        headerRef.current.classList.remove('sticky__header');
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleStickyHeader();
+    return () => window.removeEventListener('scroll', handleStickyHeader);
+  }, []);
+
+  const toggleMenu = () => setMenuVisible(!menuVisible);
+
   return (
-    <header className="header flex items-center">
-      <div className="container">
+    <header className={`header ${menuVisible ? 'menu-open' : ''}`} ref={headerRef}>
+      <div className="container bg-gradient-to-r from-indigo-400 to-cyan-400">
         <div className="flex items-center justify-between">
-          {/* ----------------logo--------------- */}
+          {/* Logo */}
           <div>
-            <img src={logo} style={{height:100, width:150}}/>
+            <img src={logo} style={{ height: 100, width: 150 }} alt="Logo" />
           </div>
 
-          {/* ----------------menu--------------- */}
-          <div className="navigation">
+          {/* Navigation Menu */}
+          <div className={`navigation ${menuVisible ? 'show__menu' : ''}`} ref={menuRef}>
             <ul className="menu flex items-center gap-[2.7rem]">
-            {
-                navLinks.map((link, index) => (
-                  <li key={index}>
-                    <NavLink
-                      to={link.path}
-                      className={(navClass) =>
-                        navClass.isActive
-                          ? 'text-blue-700 text-[16px] leading-7 font-[600]'
-                          : 'text-textColor text-[16px] leading-7 font-[500] '
-            } hover:text-primaryColor
-      >
-        {link.display}
-      </NavLink>
-    </li>
-  ))
-}
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? 'text-blue-900 text-[16px] leading-7 font-[600]'
+                        : 'text-white text-[16px] leading-7 font-[500]'
+                    }
+                    onClick={() => setMenuVisible(false)}
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
           </div>
-          {/* --------------Nav Right-------------------- */}
-          <div className="flex items-center gap-4 mr-4">
-            <div>
+
+          {/* Nav Right */}
+          <div className="flex items-center gap-4">
+            <div className="hidden md:block">
               <Link to="/">
                 <figure className="w-[35px] h-[35px] rounded-full">
                   <img src={userimg} alt="" className="w-full rounded-full" />
@@ -64,16 +86,22 @@ const Header = () => {
               </Link>
             </div>
 
-              <Link to="/login">
-                  <button className="bg-blue-500 py-1 px-6 rounded-2xl text-white font-[600] h-[44px] justify-center">Login</button>        
-              </Link>
+            {/* Login Button */}
+            <Link to="/login">
+              <button className="bg-blue-500 py-1 px-6 rounded-2xl text-white font-[600] h-[44px]">
+                Login
+              </button>
+            </Link>
 
+            {/* Mobile Menu Icon */}
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6" />
+            </span>
           </div>
-
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
